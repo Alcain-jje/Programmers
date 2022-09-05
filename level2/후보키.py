b2d6025ed2b0cb73e45d37dae6e8c73561670b02
jieun <ox804@naver.com>
2022-09-05 14:36:36 +0900

    none

--- level2/후보키.py
+++ level2/후보키.py
@@ -22,15 +22,3 @@ def solution(relation):
 relation=[["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
 print(solution(relation))
 
-
-# 0 1, 1 2, 2 3
-# 0 2, 1 3, 2 4
-
-
-
-        #print(j,i,i+2)
-        #print(j,i,i+1)
-
-"""c = combinations(relation, i)
-for j in c:
-    print(j)"""
