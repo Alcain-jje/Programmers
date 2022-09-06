58d62129619f3e979ad172a378b0c7466bf34ba5
jieun <ox804@naver.com>
2022-09-06 10:50:00 +0900

    none

--- level2/위장.py
+++ level2/위장.py
@@ -1,6 +1,5 @@
 import collections
 from itertools import combinations
-
 def solution(clothes):
     dic=collections.defaultdict(list)
     for i in range(len(clothes)):
@@ -12,8 +11,15 @@ def solution(clothes):
 
 #x3 + (a+b+c)x2 + (ab+bc+ca)x + (abc) -> (x+a)(x+b)(x+c) -> (1+a)(1+b)(1+c) - 1
 
-#clothes=[["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
-clothes=[["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
+def solution(clothes):
+    from collections import Counter
+    from functools import reduce
+    cnt= Counter([kind for name, kind in clothes])
+    answer= reduce(lambda x,y:x*(y+1),cnt.values(),1) -1
+    return answer
+
+clothes=[["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+#clothes=[["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
 
 print(solution(clothes))
 
